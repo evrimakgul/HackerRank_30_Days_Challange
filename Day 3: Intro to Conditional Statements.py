c03cea29a5034c0a0bc2ed02ee83e9c8db4d1a0a
Evrim <48595251+evrimakgul@users.noreply.github.com>
2020-10-24 22:03:44 +0200

    Create Day 3: Intro to Conditional Statements.py

---
+++ Day 3: Intro to Conditional Statements.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+
+    if N % 2 == 1:
+        print('Weird')
+    elif N % 2 == 0 and N in [2,3,4,5]:
+        print('Not Weird')
+    elif N % 2 == 0 and N in [_ for _ in range(6,21)]:
+        print('Weird')
+    elif N % 2 == 0 and N > 20:
+        print('Not Weird')
