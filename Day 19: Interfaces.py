ad5383c72c526bdb09dee88d38bcfef055402ec6
Evrim <48595251+evrimakgul@users.noreply.github.com>
2022-08-04 10:48:15 +0900

    Create Day 19: Interfaces.py

---
+++ Day 19: Interfaces.py
@@ -0,0 +1,20 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        constraint_max = 1000
+        divisors = list()
+        for divisor in range(1, 1+constraint_max):
+            if n % divisor == 0:
+                divisors.append(divisor)
+        
+        return sum(divisors)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
